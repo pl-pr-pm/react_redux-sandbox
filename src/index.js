72b75fade098d0cda1dc8875593b8e66b4fff432
rito <59119963+pl-pr-pm@users.noreply.github.com>
2022-01-11 16:17:45 +0900

    section3終了

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from "react";
 import { render } from "react-dom";
 
-var JSX = <h1>Hello world2</h1>;
-console.log(JSX);
+const returnStrings = () => {
+  return "ReturnString";
+};
 
-render(JSX, document.getElementById("root"));
+const title = "Hello world2";
+const body = "こちらが本文です";
+var reactElement = (
+  <>
+    <p>{returnStrings()}</p>
+    <h1>{title}</h1>
+    <p>{body}</p>
+  </>
+); // JSXにはObjectが格納されている
+
+/**
+ * console.log(reactElement);
+ * {type: "h1", key: null, ref: null, props: Object, _owner: null…}
+type: "h1"
+key: null
+ref: null
+props: Object
+_owner: null
+_store: Object
+ */
+
+render(reactElement, document.getElementById("root"));
