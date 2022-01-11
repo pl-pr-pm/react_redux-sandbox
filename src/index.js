ba1f4db584f07807669ca48b22ad32f7fda9e53e
rito <59119963+pl-pr-pm@users.noreply.github.com>
2022-01-11 17:31:58 +0900

    ステートを利用した簡単なアプリ作成

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 import React from "react";
 import { render } from "react-dom";
 
-class Human extends React.Component {
+import "./App.css";
+
+class H2O extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "ito" };
+    this.state = { temp: 15 };
   }
   render() {
+    const { temp } = this.state;
     return (
-      <h2>
-        {this.state.name} {this.props.age}
-      </h2>
+      <div className={this.H2OState(temp)}>
+        <h2>
+          phase: {this.H2OState(temp)}, {temp}度
+        </h2>
+        <button onClick={this.onButtonUpClick}>+</button>
+        <button onClick={this.onButtonDownClick}>-</button>
+        <button onClick={this.onButtonUp10Click}>+10</button>
+        <button onClick={this.onButtonDown10Click}>-10</button>
+      </div>
     );
   }
+  onButtonUpClick = () => {
+    this.setState({ temp: this.state.temp + 1 });
+  };
+  onButtonDownClick = () => {
+    this.setState({ temp: this.state.temp - 1 });
+  };
+  onButtonUp10Click = () => {
+    this.setState({ temp: this.state.temp + 10 });
+  };
+  onButtonDown10Click = () => {
+    this.setState({ temp: this.state.temp - 10 });
+  };
+  H2OState = (temp) => {
+    if (temp < 0) return "ice";
+    if (temp >= 100) return "steam";
+    return "water";
+  };
 }
 
-render(<Human age="30" />, document.getElementById("root"));
+render(<H2O />, document.getElementById("root"));
