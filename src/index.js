571cb22e9ef7f07ec91c2b336f1b8a800a57bb1b
rito <59119963+pl-pr-pm@users.noreply.github.com>
2022-01-12 16:25:00 +0900

    codesandbox不良のため一旦コミット

--- src/index.js
+++ src/index.js
@@ -1,62 +1,32 @@
 import React from "react";
 import { render } from "react-dom";
-import axios from "axios";
+import { Provider } from "react-redux";
+import { App } from "./App";
+import { createStore } from "redux";
+import { reducer } from "./reducer";
 
-import "./App.css";
+const store = createStore(reducer);
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { giphyList: [], inputText: "" };
-  }
+//
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-  // componentDidMount() {
-  //   //ライフサイクルメソッド： Appコンポーネントがレンダーされたタイミングで実行
-  //   this.giphyApi();
-  // }
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
-  renderImageList(list) {
-    const imageList = list.map((url) => {
-      return (
-        <li className="item">
-          <img className="image" src={url} />
-        </li>
-      );
-    });
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-    return <ul className="list">{imageList}</ul>;
-  }
+//   render() {
+//     return <></>;
+//   }
+// }
 
-  onChange = (e) => {
-    this.setState({ inputText: e.target.value });
-  };
-
-  onClick = () => {
-    this.giphyApi(this.state.inputText);
-    this.setState({ inputText: "" });
-  };
-
-  render() {
-    return (
-      <>
-        <input type="text" value={this.inputText} onChange={this.onChange} />
-        <button onClick={this.onClick}>SEARCH</button>
-        {this.renderImageList(this.state.giphyList)}
-      </>
-    );
-  }
-  giphyApi = (searchText) => {
-    const search = searchText;
-    const key = "V6AU97qCSCYVmbIC5UDppEiVM1xnuO9E";
-    const limit = 3;
-
-    const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=${limit}`;
-    axios.get(url).then((res) => {
-      const data = res.data.data;
-      const imageUrl = data.map((item) => item.images.downsized.url);
-      this.setState({ giphyList: imageUrl });
-    });
-  };
-}
-
-render(<App />, document.getElementById("root"));
+// render(<App />, document.getElementById("root"));
---
+++ src/reducer.js
@@ -0,0 +1,10 @@
+export const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "PLUS":
+      return state + action.payload.num;
+    case "MINUS":
+      return state - action.payload.num;
+    default:
+      return state;
+  }
+};
