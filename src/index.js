f00b9a02ebd23184212b3949903f24316de642c4
rito <59119963+pl-pr-pm@users.noreply.github.com>
2022-01-12 13:27:40 +0900

    giphyからgifを取得するAPP

--- src/index.js
+++ src/index.js
@@ -2,36 +2,59 @@ import React from "react";
 import { render } from "react-dom";
 import axios from "axios";
 
+import "./App.css";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { member: [] };
+    this.state = { giphyList: [], inputText: "" };
   }
 
-  memberList(list) {
-    const memberList = list.map((member) => {
+  // componentDidMount() {
+  //   //ライフサイクルメソッド： Appコンポーネントがレンダーされたタイミングで実行
+  //   this.giphyApi();
+  // }
+
+  renderImageList(list) {
+    const imageList = list.map((url) => {
       return (
-        <li>
-          {member.name} {member.age}
+        <li className="item">
+          <img className="image" src={url} />
         </li>
       );
     });
-    return <ul>{memberList}</ul>;
+
+    return <ul className="list">{imageList}</ul>;
   }
+
+  onChange = (e) => {
+    this.setState({ inputText: e.target.value });
+  };
+
+  onClick = () => {
+    this.giphyApi(this.state.inputText);
+    this.setState({ inputText: "" });
+  };
 
   render() {
     return (
       <>
-        <button onClick={this.getJson}>request</button>
-        {this.memberList(this.state.member)}
+        <input type="text" value={this.inputText} onChange={this.onChange} />
+        <button onClick={this.onClick}>SEARCH</button>
+        {this.renderImageList(this.state.giphyList)}
       </>
     );
   }
+  giphyApi = (searchText) => {
+    const search = searchText;
+    const key = "V6AU97qCSCYVmbIC5UDppEiVM1xnuO9E";
+    const limit = 3;
 
-  getJson = () => {
-    axios.get("https://myjson.dit.upm.es/api/bins/6xen").then((res) => {
-      console.log(res.data);
-      this.setState(res.data);
+    const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=${limit}`;
+    axios.get(url).then((res) => {
+      const data = res.data.data;
+      const imageUrl = data.map((item) => item.images.downsized.url);
+      this.setState({ giphyList: imageUrl });
     });
   };
 }
