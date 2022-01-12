96f2b95a993dd8affaca9c5539f61036909cff75
rito <59119963+pl-pr-pm@users.noreply.github.com>
2022-01-12 12:14:14 +0900

    外部APIとの通信

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import React from "react";
 import { render } from "react-dom";
-import { TodoApp } from "./TodoApp";
+import axios from "axios";
 
-import "./App.css";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { member: [] };
+  }
 
-render(<TodoApp />, document.getElementById("root"));
+  memberList(list) {
+    const memberList = list.map((member) => {
+      return (
+        <li>
+          {member.name} {member.age}
+        </li>
+      );
+    });
+    return <ul>{memberList}</ul>;
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={this.getJson}>request</button>
+        {this.memberList(this.state.member)}
+      </>
+    );
+  }
+
+  getJson = () => {
+    axios.get("https://myjson.dit.upm.es/api/bins/6xen").then((res) => {
+      console.log(res.data);
+      this.setState(res.data);
+    });
+  };
+}
+
+render(<App />, document.getElementById("root"));
